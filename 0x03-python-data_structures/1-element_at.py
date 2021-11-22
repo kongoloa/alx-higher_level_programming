7538dfbbe9ed48984eca05dbb9a1cdd5864c97af
kongoloa <aristoteshako@gmail.com>
2021-11-22 12:09:21 -0800

    get element from list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def element_at(x, y):
-    if y < 0 or y> len(x):
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
         return None
     else:
-        return x[y]
+        return my_list[idx]
