94997ffd36395a21d773ec45e8a43316a9daccfe
kongoloa <aristoteshako@gmail.com>
2021-11-24 19:12:15 -0800

    biggest int value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def best_score(dict):
-    if not dict:
-        return None
-    return max(dict)
+    if dict and len(dict):
+        max = list(dict.keys())[0]
+        for key in dict:
+            if dict[key] > dict[max]:
+                max = key
+        return max
+    return None
