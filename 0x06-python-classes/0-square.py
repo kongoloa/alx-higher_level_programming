26c79c4a7218c49ca306607207a2b6f226003099
kongoloa <aristoteshako@gmail.com>
2021-12-07 17:21:59 -0800

    an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Square class definition"""
+"""
+Square class definition
+"""
 
 class Square:
-    """An empty Square class"""
+    """
+    An empty Square class
+    """
     pass
