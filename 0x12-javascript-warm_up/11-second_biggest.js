08ecde68748c5973620b32ef6c6723f7f582f19b
kongoloa <aristoteshako@gmail.com>
2022-02-28 15:22:09 -0800

    2nd biggest int

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * searches the second biggest integer in the list of arguments.
+ */
+
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  console.log(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
+}
