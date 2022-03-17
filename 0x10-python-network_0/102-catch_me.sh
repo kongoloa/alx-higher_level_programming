6f4d0e11e6c5c221d515dfea3a9d294561b19bac
kongoloa <aristoteshako@gmail.com>
2022-03-17 13:57:12 -0700

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-
-curl -sL 0.0.0.0:5000/catch_me -X PUT "You got me!" -H "Origin: HolbertonSchool" -d "user_id=98"
+curl -sL -X PUT -H "You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
